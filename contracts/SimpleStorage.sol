a395ac89cd6b1370d1251e70fb323b11b872918e
Aniket Mandloi <aniketmandloi@Anikets-MacBook-Air.local>
2024-07-24 22:50:32 +0530

    simplified the contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,15 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract SimpleStorage {
-    uint256 private storedData;
+    uint256 public storedData;
 
-    // Function to set the value of the storedData
     function set(uint256 x) public {
         storedData = x;
-    }
-
-    // Function to get the value of the storedData
-    function get() public view returns (uint256) {
-        return storedData;
     }
 }
